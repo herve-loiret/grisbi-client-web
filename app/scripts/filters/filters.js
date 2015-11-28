49c04a0b554c84f8a0c3abc742c06dc97675adcb
Herve <herve.loiret@gmail.com>
2015-11-28 17:31:27 +0100

    deleting console.log

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -1,18 +1,16 @@
 angular.module('grisbiClientWebApp').filter('accountByType', function() {
-  return function(accounts,types) {
-    console.log(accounts);
-    console.log(types);
+    return function(accounts, types) {
 
-    var out = [];
+	var out = [];
 
-    angular.forEach(accounts, function(account){
-      angular.forEach(types, function(type){
-        if(type === account.typeAccount){
-          out.push(account);
-        }
-      });
-    });
+	angular.forEach(accounts, function(account) {
+	    angular.forEach(types, function(type) {
+		if (type === account.typeAccount) {
+		    out.push(account);
+		}
+	    });
+	});
 
-    return out;
-  }
+	return out;
+    }
 });
