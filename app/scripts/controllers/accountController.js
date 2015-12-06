08f6f061f1eea1b3eba0391160fc87cf63119fc6
Herve <herve.loiret@gmail.com>
2015-12-07 00:21:28 +0100

    adding some pagination stuff

--- app/scripts/controllers/accountController.js
+++ app/scripts/controllers/accountController.js
@@ -42,17 +42,17 @@ angular.module('grisbiClientWebApp').controller('accountController', function($f
     }
 
     // gestion de la pagination
-    $scope.totalItems = 64;
-    $scope.currentPage = 4;
     $scope.setPage = function (pageNo) {
       $scope.currentPage = pageNo;
     };
     $scope.pageChanged = function() {
       console.log('Page changed to: ' + $scope.currentPage);
     };
-    $scope.maxSize = 5;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
+    
+    $scope.maxSize = 10;
+    $scope.totalItems = 50;
+    $scope.currentPage = 3;
+    $scope.itemsPerPage = 5;
     
     // gestion du datepicker :
     $scope.open = function($event) {
